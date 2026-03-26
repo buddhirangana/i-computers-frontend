abd51950e0f5accc79c5e9b1a85d34b33648d076
Buddhi Rangana <buddhirangana1999@gmail.com>
2026-03-26 21:09:14 +0530

    day 20 🚀

--- src/pages/admin/adminProductPage.jsx
+++ src/pages/admin/adminProductPage.jsx
@@ -1,99 +1,139 @@
 import { Link } from "react-router-dom";
 import { FaPlus } from "react-icons/fa6";
 import { useEffect, useState } from "react";
+import { TbTrash } from "react-icons/tb";
+import { TbEdit } from "react-icons/tb";
+import { toast } from "react-hot-toast";
 import axios from "axios";
-export default function AdminProductsPage(){
+import LoadingAnimation from "../../components/loadingAnimation";
+import ProductDeleteModel from "../../components/productDeleteModel";
 
-    const [products , setProducts] = useState([])
+export default function AdminProductsPage() {
+
+    const [products, setProducts] = useState([]);
+    const [isProductsAreLoaded, setIsProductsAreLoaded] = useState(false);
+
     useEffect(
-        ()=>{
-                    //backend api
-            const token = localStorage.getItem("token")
+        () => {
+            //backend api
+            if (!isProductsAreLoaded) {
+                const token = localStorage.getItem("token")
 
-            axios.get(import.meta.env.VITE_API_URL+"/products",{
-                headers:{
-                    "Authorization": "Bearer "+ token
-                }
-            }).then(
-                (response)=>{
-                    setProducts(response.data)
-                }
-            ).catch(
-                (error)=>{
-                    console.log(error)
-                }
-            )
-        },
-        []
-    )
-
-    
-
-    return(
-        <div className="w-full h-full overflow-y-scroll p-5">
-
-            {/* {
-                products.map(
-                    (item , index)=>{
-
-                        return <p key={index}>
-                            {item.productId}
-                        </p>
-
+                axios.get(import.meta.env.VITE_API_URL + "/products", {
+                    headers: {
+                        "Authorization": "Bearer " + token
+                    }
+                }).then(
+                    (response) => {
+                        setProducts(response.data);
+                        setIsProductsAreLoaded(true);
+                    }
+                ).catch(
+                    (error) => {
+                        console.log(error)
                     }
                 )
-            } */}
+            }
+        },
+        [isProductsAreLoaded]
+    )
+
+
+
+    return (
+        <div className="w-full h-full overflow-y-scroll p-5">
             <div className="sticky top-0 w-full h-[100px] rounded-lg bg-accent text-white flex items-center p-5 justify-between shadow-2xl">
-                <h1 className="text-2xl  font-semibold">Products</h1>                
+                <h1 className="text-2xl  font-semibold">Products</h1>
             </div>
 
-            <table className="mt-5 w-full text-secondary ">
-                <thead className="bg-accent/45 text-white">
-                    <tr>
-                        <th className="text-center border border-primary p-4">Image</th>
-                        <th className="text-center border border-primary p-4">Product ID</th>
-                        <th className="text-center border border-primary p-4">Name</th>
-                        <th className="text-center border border-primary p-4">Price</th>
-                        <th className="text-center border border-primary p-4">Labelled Price</th>
-                        <th className="text-center border border-primary p-4">Brand</th>
-                        <th className="text-center border border-primary p-4">Model</th>
-                        <th className="text-center border border-primary p-4">Category</th>
-                        <th className="text-center border border-primary p-4">Availability</th>
-                        <th className="text-center border border-primary p-4">Stock</th>
-                    </tr>
-                </thead>
+            {
+                isProductsAreLoaded ?
+                    <table className="mt-5 w-full text-secondary ">
+                        <thead className="bg-accent/45 text-white">
+                            <tr>
+                                <th className="text-center border border-primary p-4">Image</th>
+                                <th className="text-center border border-primary p-4">Product ID</th>
+                                <th className="text-center border border-primary p-4">Name</th>
+                                <th className="text-center border border-primary p-4">Price</th>
+                                <th className="text-center border border-primary p-4">Labelled Price</th>
+                                <th className="text-center border border-primary p-4">Brand</th>
+                                <th className="text-center border border-primary p-4">Model</th>
+                                <th className="text-center border border-primary p-4">Category</th>
+                                <th className="text-center border border-primary p-4">Availability</th>
+                                <th className="text-center border border-primary p-4">Stock</th>
+                                <th className="text-center border border-primary p-4">Actions</th>
+                            </tr>
+                        </thead>
 
-                <tbody>
-                    {
+                        <tbody>
+                            {
 
-                        products.map(
-                            (item)=>{
-                                return(
-                                    <tr className="odd:bg-gray-600 even:bg-primary odd:text-white border-t-4 border-primary hover:bg-accent/45" key={item.productId}>
-                                        <td className="p-2">
-                                            <img src={item.images[0]} alt={item.name} className="w-16 h-16 object-cover rounded-full" />
-                                        </td>
-                                        <td className="text-center text-wrap p-2">{item.productId}</td>
-                                        <td className="text-center text-wrap p-2">{item.name}</td>
-                                        <td className="text-center text-wrap p-2">{item.price}</td>
-                                        <td className="text-center text-wrap p-2">{item.labelledPrice}</td>
-                                        <td className="text-center text-wrap p-2">{item.brand}</td>
-                                        <td className="text-center text-wrap p-2">{item.model}</td>
-                                        <td className="text-center text-wrap p-2">{item.category}</td>
-                                        <td className="text-center text-wrap p-2"></td>
-                                        <td className="text-center text-wrap p-2">{item.stock}</td>
-                                    </tr>
+                                products.map(
+                                    (item) => {
+                                        return (
+                                            <tr className="odd:bg-gray-600 even:bg-primary odd:text-white border-t-4 border-primary hover:bg-accent/45" key={item.productId}>
+                                                <td className="p-2">
+                                                    <img src={item.images[0]} alt={item.name} className="w-16 h-16 object-cover rounded-full" />
+                                                </td>
+                                                <td className="text-center text-wrap p-2">{item.productId}</td>
+                                                <td className="text-center text-wrap p-2">{item.name}</td>
+                                                <td className="text-center text-wrap p-2">{item.price}</td>
+                                                <td className="text-center text-wrap p-2">{item.labelledPrice}</td>
+                                                <td className="text-center text-wrap p-2">{item.brand}</td>
+                                                <td className="text-center text-wrap p-2">{item.model}</td>
+                                                <td className="text-center text-wrap p-2">{item.category}</td>
+                                                <td className="text-center text-wrap p-2"></td>
+                                                <td className="text-center text-wrap p-2">{item.stock}</td>
+                                                <td className="text-center p-2">
+                                                    {/* <TbTrash className="text-2xl text-red-500 hover:text-red-700 cursor-pointer"
+                                                        onClick={
+                                                            () => {
+                                                                const token = localStorage.getItem("token");
+                                                                axios.delete(import.meta.env.VITE_API_URL + "/products/" + item.productId, {
+                                                                    headers: {
+                                                                        "Authorization": "Bearer " + token
+                                                                    }
+                                                                }).then(
+                                                                    () => {
+                                                                        toast.success("Product deleted successfully");
+                                                                        setIsProductsAreLoaded(false);
+                                                                    }
+                                                                ).catch(
+                                                                    (error) => {
+                                                                        toast.error("Failed to delete product");
+                                                                        console.log(error);
+                                                                    }
+                                                                )
+                                                            }
+                                                        } /> */}
+
+                                                    <ProductDeleteModel
+                                                        product={item}
+                                                        refresh={
+                                                            () => {
+                                                                setIsProductsAreLoaded(false);
+                                                            }
+                                                        }
+                                                    />
+                                                    <Link to="/admin/edit-product" state={item}>
+                                                        <TbEdit className="text-2xl text-blue-500 hover:text-blue-700 cursor-pointer" />
+                                                    </Link>
+                                                </td>
+                                            </tr>
+                                        )
+                                    }
                                 )
+
                             }
-                        )
 
-                    }                   
+                        </tbody>
 
-                </tbody>
+                    </table>
+                    :
+                    <LoadingAnimation />
+            }
 
-            </table>
-
-            <Link to="/admin/add-product" 
+            <Link to="/admin/add-product"
                 className="fixed bottom-8 right-8 w-[60px] h-[60px] bg-accent flex justify-center items-center text-white text-3xl rounded-full shadow-2xl hover:bg-black hover:text-accent">
                 <FaPlus />
             </Link>
